6f52f35e3f426f18192d5d91c3a31c81bb5197c7
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2024-02-08 14:49:21 +0700

    Changing utils logo string

--- utils.py
+++ utils.py
@@ -2,5 +2,6 @@ from streamlit_extras.app_logo import add_logo
 
 def init():
     add_logo(
-        "logo.png"
+        # "logo.png"
+        "https://raw.githubusercontent.com/ZulfiFazhar/PSD-UAS-IF1-FOLIUM/main/LOGO.png"
     )
